8d0c2c8718f2a40431916da322cd211fce1ac7a4
vimal <vimal@cronj.com>
2018-03-29 11:40:42 +0530

    bold title

--- xls.py
+++ xls.py
@@ -3,17 +3,22 @@ import xlsxwriter
 
 class Xcel:
     def __init__(self, name, schema):
-      self.name = name + '.xlsx'
+      self.name = 'reports/' + name + '.xlsx'
       self.schema = schema
       self.workbook = xlsxwriter.Workbook(self.name)
+      self.style = {
+          'bold': self.workbook.add_format({'bold': True})
+      }
+      self.ALPHABETS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
       self.worksheet = self.workbook.add_worksheet()
       self.row = 0
       self.columns = []
       col = 0
       i = 0
       for colVal, colName in self.schema.items():
+          colIndex = self.ALPHABETS[i] + '1'
           self.columns.append(colVal)
-          self.worksheet.write(self.row, col + i, colName)
+          self.worksheet.write(colIndex, colName, self.style['bold'])
           i += 1
       i = 0
       self.row += 1
@@ -22,10 +27,13 @@ class Xcel:
       row = self.row
       col = 0
       for colVal in self.columns:
+        try:
           colVal = str(colVal)
           value = str(data[colVal])
           self.worksheet.write(row, col, value)
-          col += 1
+        except:
+          pass
+        col += 1
       self.row += 1
       return self
 
